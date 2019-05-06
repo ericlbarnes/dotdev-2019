a62eca4b15ebcc831d092ebfa448dee9c3b46f59
Jason Beggs <jasonlbeggs@gmail.com>
2019-05-06 17:16:45 -0400

    Disable PurgeCSS in dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,25 @@
 const mix = require('laravel-mix');
 
-mix.postCss('./css/style.css', './', [
-  require('postcss-import'),
-  require('tailwindcss'),
-  require('@fullhuman/postcss-purgecss')({
-    content: [
-      './**.php',
-      './**/**.php',
-      './**.html',
-      './**.js',
-    ],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-  }),
-]);
+mix.postCss('./css/style.css', './')
+  .options({
+    postCss: [
+      require('postcss-import'),
+      require('tailwindcss'),
+    ]
+  });
+
+if(mix.inProduction()) {
+  mix.options({
+    postCss: [
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+          './**.php',
+          './**/**.php',
+          './**.html',
+          './**.js',
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      }),
+    ]
+  });
+}
